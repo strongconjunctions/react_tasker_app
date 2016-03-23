56afb46d6c96df57ef055f3389d4783404e1e34c
strongconjunctions <strongconjunctions@gmail.com>
2016-03-23 11:49:59 +0000

    minor changes to the UI

--- src/app/components/TaskItem.js
+++ src/app/components/TaskItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class TaskItem extends React.Component {
     render() {
         return (
-            <li>This task is some whacky ass shit!</li>
+            <li>Another task bytes the dust!</li>
         )
     }
 }
